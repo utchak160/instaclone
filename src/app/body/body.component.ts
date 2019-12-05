203a01a1cb6a34116100c06da930d1897d0c14bf
utchak160 <chakravartytutkarsh@gmail.com>
2019-12-05 12:05:44 +0530

    body modified

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cards } from '../Cards';
+import {ifTrue} from "codelyzer/util/function";
+import {timeInterval} from "rxjs/operators";
 
 @Component({
   selector: 'app-body',
@@ -9,6 +11,7 @@ import { Cards } from '../Cards';
 export class BodyComponent implements OnInit {
   Cards = Cards;
   touched = false;
+  clicked = false;
 
   constructor() { }
 
@@ -20,4 +23,8 @@ export class BodyComponent implements OnInit {
   saved() {
     alert('Successfully added in your saved list!');
   }
+
+  // like() {
+  //   this.clicked = true;
+  // }
 }
